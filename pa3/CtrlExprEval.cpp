5c5a49812fd483308c946fadd4868d687d49cbfa
liux0229 <liux0229@gmail.com>
2013-06-10 03:23:08 -0700

    More operators

--- pa3/CtrlExprEval.cpp
+++ pa3/CtrlExprEval.cpp
@@ -107,13 +107,118 @@ UTokenLiteral operator%(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
   }
 }
 
+UTokenLiteral operator+(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    lhs->toUnsigned64() + rhs->toUnsigned64());
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    lhs->toSigned64() + rhs->toSigned64());
+  }
+}
+
+UTokenLiteral operator-(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    lhs->toUnsigned64() - rhs->toUnsigned64());
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    lhs->toSigned64() - rhs->toSigned64());
+  }
+}
+
+unsigned long long getShiftRHS(const UTokenLiteral& rhs) {
+  unsigned long long r;
+  if (rhs->isSigned()) {
+    long long sr = rhs->toSigned64();
+    if (sr < 0) {
+      Throw("Trying to left/right shift by {}", sr);
+    }
+    r = sr;
+  } else {
+    r = rhs->toUnsigned64();
+  }
+  if (r >= 64) {
+    Throw("Left/right shift RHS operand too large: {}", r);
+  }
+  return r;
+}
+
+UTokenLiteral leftShift(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  unsigned long long r = getShiftRHS(rhs);
+  if (lhs->isSigned()) {
+    return getToken(FT_LONG_LONG_INT, lhs->toSigned64() << r);
+  } else {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, lhs->toUnsigned64() << r);
+  }
+}
+
+UTokenLiteral operator>>(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  unsigned long long r = getShiftRHS(rhs);
+  if (lhs->isSigned()) {
+    return getToken(FT_LONG_LONG_INT, lhs->toSigned64() >> r);
+  } else {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, lhs->toUnsigned64() >> r);
+  }
+}
+
+// TODO: we can turn this into a combination of the base structure plus a
+// functor
+UTokenLiteral operator<(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    (unsigned long long)(lhs->toUnsigned64() < rhs->toUnsigned64()));
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    (long long)(lhs->toSigned64() < rhs->toSigned64()));
+  }
+}
+
+UTokenLiteral operator<=(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    (unsigned long long)(lhs->toUnsigned64() <= rhs->toUnsigned64()));
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    (long long)(lhs->toSigned64() >= rhs->toSigned64()));
+  }
+}
+
+UTokenLiteral operator>(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    (unsigned long long)(lhs->toUnsigned64() > rhs->toUnsigned64()));
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    (long long)(lhs->toSigned64() > rhs->toSigned64()));
+  }
+}
+
+UTokenLiteral operator>=(const UTokenLiteral& lhs, const UTokenLiteral& rhs) {
+  if (!lhs->isSigned() || !rhs->isSigned()) {
+    return getToken(FT_UNSIGNED_LONG_LONG_INT, 
+                    (unsigned long long)(lhs->toUnsigned64() >= rhs->toUnsigned64()));
+  } else {
+    return getToken(FT_LONG_LONG_INT, 
+                    (long long)(lhs->toSigned64() >= rhs->toSigned64()));
+  }
+}
+
 const map<ETokenType, 
           function<UTokenLiteral (const UTokenLiteral&,
                                   const UTokenLiteral&)>
          > binaryOps = {
   { OP_STAR, operator* },
   { OP_DIV,  operator/ },
-  { OP_MOD,  operator% }
+  { OP_MOD,  operator% },
+  { OP_PLUS,  operator+ },
+  { OP_MINUS,  operator- },
+  { OP_LSHIFT, leftShift }, // somehow using operator<< does not work
+  { OP_RSHIFT, operator>> },
+  { OP_LT, operator< },
+  { OP_LE, operator<= },
+  { OP_GT, operator> },
+  { OP_GE, operator>= }
 };
 
 UTokenLiteral apply(ETokenType type, const UTokenLiteral& token)
@@ -263,8 +368,8 @@ public:
   UTokenLiteral mul() {
     UTokenLiteral lhs = unary();
 
-    while (isOp(OP_STAR)  ||
-           isOp(OP_DIV) ||
+    while (isOp(OP_STAR) ||
+           isOp(OP_DIV)  ||
            isOp(OP_MOD)) {
       ETokenType op = getOp();
       next();
@@ -275,9 +380,53 @@ public:
     return lhs;
   }
 
+  UTokenLiteral add() {
+    UTokenLiteral lhs = mul();
+
+    while (isOp(OP_PLUS) ||
+           isOp(OP_MINUS)) {
+      ETokenType op = getOp();
+      next();
+      UTokenLiteral rhs = mul();
+      lhs = apply(op, lhs, rhs);
+    }
+
+    return lhs;
+  }
+
+  UTokenLiteral shift() {
+    UTokenLiteral lhs = add();
+
+    while (isOp(OP_LSHIFT) ||
+           isOp(OP_RSHIFT)) {
+      ETokenType op = getOp();
+      next();
+      UTokenLiteral rhs = add();
+      lhs = apply(op, lhs, rhs);
+    }
+
+    return lhs;
+  }
+
+  UTokenLiteral relational() {
+    UTokenLiteral lhs = shift();
+
+    while (isOp(OP_LT) ||
+           isOp(OP_LE) ||
+           isOp(OP_GE) ||
+           isOp(OP_GT)) {
+      ETokenType op = getOp();
+      next();
+      UTokenLiteral rhs = shift();
+      lhs = apply(op, lhs, rhs);
+    }
+
+    return lhs;
+  }
+
   UTokenLiteral control() {
     // check left over tokens
-    return mul();
+    return relational();
   }
 
   UTokenLiteral parse() {
--- pa3/PostTokenReceiver.h
+++ pa3/PostTokenReceiver.h
@@ -16,15 +16,23 @@ public:
   }
 
   void put(const PostToken& token) {
-    if (token.getType() != PostTokenType::NewLine) {
-      // printToken(token); 
+    // debug(token);
+    if (token.getType() != PostTokenType::Eof) {
+      send_(token);
+    } else {
+      printToken(token);
     }
-    send_(token);
   }
 
 private:
   void printToken(const PostToken& token) const {
     std::cout << token.toStr() << std::endl;
+  }
+
+  void debug(const PostToken& token) const {
+    if (token.getType() != PostTokenType::NewLine) {
+      printToken(token); 
+    }
   }
 
   std::function<void (const PostToken&)> send_;

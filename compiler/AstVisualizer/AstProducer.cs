42676fe7a2698a72e84a14d534af1ad58fe5b0a9
Rocky Liu <liux0229@gmail.com>
2014-08-17 23:28:53 -0700

    AST visualizer bug fix.

--- compiler/AstVisualizer/AstProducer.cs
+++ compiler/AstVisualizer/AstProducer.cs
@@ -20,21 +20,30 @@ namespace AstVisualizer
         {
             var input = Path.GetTempFileName();
             var output = Path.GetTempFileName();
+            var resultFile = Path.GetTempFileName();
             File.WriteAllText(input, m_Source);
 
+            // we need to perform the following dance to be able to redirect the stderr to a file
+            // (asynchronously read from RedirectStandardError does not interact well with
+            // the async pattern below)
             var parser = @"C:\PortableDocuments\dev\compiler\cppcompiler\compiler\x64\Debug\recog.exe";
-            var arguments = string.Format("-o {0} {1}", output, input);
-            var result = await getParserResult(parser, arguments);
+            var arguments = string.Format("/c \"{0} -o {1} {2}\" 2> {3}", parser, output, input, resultFile);
+            var result = await getParserResult("cmd.exe", arguments, resultFile);
             return createAst(result);
         }
 
         private AstNode createAst(string result)
         {
             var lines = result.Split(Environment.NewLine.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length == 0)
+            {
+                lines = new string[] { "[Error]" };
+            }
+            
             return AstGenerator.get(lines).First();
         }
 
-        private Task<string> getParserResult(string command, string arguments)
+        private Task<string> getParserResult(string command, string arguments, string resultFile)
         {
             var tcs = new TaskCompletionSource<string>();
 
@@ -46,20 +55,24 @@ namespace AstVisualizer
                     Arguments = arguments,
                     CreateNoWindow = true, // do not show the shell window
                     UseShellExecute = false,
-                    RedirectStandardError = true,
                     WindowStyle = ProcessWindowStyle.Hidden,
                 },
                 EnableRaisingEvents = true
             };
 
+            StringBuilder result = new StringBuilder();
             process.Exited += (sender, args) =>
             {
-                var result = process.StandardError.ReadToEnd();
-                tcs.SetResult(result);
+                tcs.SetResult(File.ReadAllText(resultFile));
                 process.Dispose();
             };
-
+            
             process.Start();
+
+            // Note: the following is for debugging only.
+            // But I noticed this would trigger a 'completing a task multiple time'
+            // exception for unknown reasons.
+            // process.WaitForExit();
 
             return tcs.Task;
         }
